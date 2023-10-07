cc2c8cf2ca8339068c1e87bd354d482d76c3ecab
ankittkamal <officiallyankit999@gmail.com>
2023-10-07 11:46:40 +0530

    Added Sign-in API and Error Handling

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+require("express-async-errors");
 const express = require("express");
-require("./db");
+const morgan = require("morgan");
 require("dotenv").config();
+
+require("./db");
 const userRouter = require("./routes/user");
+const { errorHandler } = require("./middlewares/error");
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
 app.use("/api/user", userRouter);
-
+app.use(errorHandler);
 app.listen(8000, () => {
   console.log("the port is running on port 8000");
 });
--- routes/user.js
+++ routes/user.js
@@ -7,17 +7,20 @@ const {
   forgetPassword,
   sendResetPasswordTokenStatus,
   resetPassword,
+  signIn,
 } = require("../controllers/user");
 const {
   userValidator,
   validate,
   validatePassword,
+  signInValidator,
 } = require("../middlewares/validator");
 const { isValidPassResetToken } = require("../middlewares/user");
 
 const router = express.Router();
 
 router.post("/create", userValidator, validate, create);
+router.post("/sign-in", signInValidator, validate, signIn);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-email-verification-token", resendEmailVerificationToken);
 router.post("/forget-password", forgetPassword);
