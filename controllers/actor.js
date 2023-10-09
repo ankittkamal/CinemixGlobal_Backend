84bf4fe4e2c0da40b141f150c1a67f17264c1eae
ankittkamal <officiallyankit999@gmail.com>
2023-10-09 17:24:39 +0530

    Created Role Based Authentication for admin and users

--- controllers/actor.js
+++ controllers/actor.js
@@ -19,7 +19,7 @@ exports.createActor = async (req, res) => {
     newActor.avatar = { url, public_id };
   }
   await newActor.save();
-  res.status(201).json(formatActor(actor));
+  res.status(201).json(formatActor(newActor));
 };
 
 // update
