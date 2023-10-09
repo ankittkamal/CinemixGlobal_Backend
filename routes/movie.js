394d488fa4e2d5a81ad1c7eb3200d37fbf2919cd
ankittkamal <officiallyankit999@gmail.com>
2023-10-09 20:50:56 +0530

    Created Update Movie API

--- routes/movie.js
+++ routes/movie.js
@@ -5,6 +5,9 @@ const { uploadVideo, uploadImage } = require("../middlewares/multer");
 const { createMovie } = require("../controllers/movie");
 const { validate, validateMovie } = require("../middlewares/validator");
 const { parseData } = require("../utils/helper");
+const { updateMovieWithoutPoster } = require("../controllers/movie");
+const { updateMovieWithPoster } = require("../controllers/movie");
+const { removeMovie } = require("../controllers/movie");
 const router = express.Router();
 
 router.post(
@@ -25,5 +28,26 @@ router.post(
   //   validate,
   createMovie
 );
+router.patch(
+  "/update-movie-without-poster/:movieId",
+  isAuth,
+  isAdmin,
+  // parseData,
+  validateMovie,
+  validate,
+  updateMovieWithoutPoster
+);
+router.patch(
+  "/update-movie-with-poster/:movieId",
+  isAuth,
+  isAdmin,
+  uploadImage.single("poster"),
+  parseData,
+  validateMovie,
+  validate,
+  updateMovieWithPoster
+);
+
+router.delete("/:movieId", isAuth, isAdmin, removeMovie);
 
 module.exports = router;
